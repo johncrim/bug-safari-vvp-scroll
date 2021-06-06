78b5b5819f7eb46c31ca8747c3ffb544d99bcc5a
John Crim <johncrim@users.noreply.github.com>
2021-06-06 10:46:28 -0700

    improve(build): using github pages

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'dist/bug-repro.js',
+  input: 'tsc-out/bug-repro.js',
   output: {
-    file: 'index.mjs',
+    file: 'dist/index.mjs',
     format: 'esm'
   },
   plugins: [nodeResolve()],
